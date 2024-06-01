21d7c0836faefefe7ed5f30d1cfc7699fde7af2a
CharlesRobertoSky <charlesdelimaroberto@gmail.com>
2024-05-31 22:35:42 -0300

    tentando fazer comunicação do socketio

---
+++ src/web/server.mjs
@@ -0,0 +1,39 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import {Server} from 'socket.io'
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server)
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'))
+});
+
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', ( )=>{
+    console.log('user disconnect')
+  });
+});
+
+io.on('connection', (socket) =>{
+  socket.broadcast.emit('hi');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  });
+});
+
+
+io.emit('za warudo')
+
+server.listen(3000, () =>{
+  console.log('Server Running at http://localhost:3000');
+})
