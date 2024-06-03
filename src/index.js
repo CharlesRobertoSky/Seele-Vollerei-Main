52d030b73f5aebeabef09153a4708c25a8ea7428
CharlesRobertoSky <charlesdelimaroberto@gmail.com>
2024-06-02 21:33:53 -0300

    removendo eventos

--- src/index.js
+++ src/index.js
@@ -44,4 +44,12 @@ client.on(Events.InteractionCreate, async interaction => {
   }
 })
 
+client.on(Events.InteractionCreate, async interaction =>{
+  if (!interaction.isChatInputCommand()) return
+  if (interaction.commandName === 'ping'){
+    await interaction.reply({content:'Secret Pong!', ephemeral: true})
+  }
+})
 
+
+g
