119af6a1eacc74f5ff43c1ff3c60c57098228fcb
CharlesRobertoSky <charlesdelimaroberto@gmail.com>
2024-07-08 23:27:39 -0300

    add help command

--- bot/Commands/Information/help.js
+++ bot/Commands/Information/help.js
@@ -3,7 +3,8 @@ const {
   EmbedBuilder,
   SlashCommandBuilder,
   AtionRowBuilder,
-  StringSeectMenuBuilder
+  StringSeectMenuBuilder,
+  StringSelectMenuBuilder
 } = require('discord.js');
 
 module.exports = {
@@ -26,5 +27,50 @@ module.exports = {
 
       return getCommandID;
     }
+    const directries = [
+      ...new Set(Client.commands.map((cmd) => cmd.folder))
+    ]
+    const formatString = (str) => 
+      `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
+    
+    const categories = directories.map((dir) => {
+      const getCommandS = client.commands
+      .filter((cmd) => cmd.folder === dir)
+      .map((cmd) => {
+        return {
+          name: cmd.data.name,
+          description: cmd.data.description || "there is no description for this command"
+        }
+      })
+      return {
+        directory: formatString(dir),
+        commands: getCommandS,
+      }
+    })
+  
+    const embed = new EmbedBuilder()
+      .setDescription("See lists of commands by sdelecting a category down below!")
+      .setColor("#235ee7")
+      .setAuthor({name: `${client.user.username} commands`, iconURL:client.user.avatarURL() })
+    
+      const components = (state) =>{
+        new ActionRowBuilder().addComponents(
+          new StringSelectMenuBuilder()
+            .setCustomId("Help-menu")
+            .setPlaceholder("find a category")
+            .setDisabled(state)
+            .addOptions(
+              categories.map((cmd) => {
+                return {
+                  label: cmd.directory,
+                  value: cmd.directory.toLowerCase(),
+                  description: `Commands from ${cmd.directory} category.`,
+                  emoji: emojis[cmd.directory.toLowerCase() || null]
+                }
+              })
+            )
+        )
+      }
+
   }
 };
